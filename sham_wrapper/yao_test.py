6b723385cf3de2d9ab6de3ea357db6b68e573861
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-30 15:44:16 -0400

    Strengthened test tolerance to illustrate error

--- sham_wrapper/yao_test.py
+++ sham_wrapper/yao_test.py
@@ -16,7 +16,7 @@ for scatter in (0.05, 0.1, 0.15, 0.2, 0.25):
     af.deconvolute(scatter)
 
 
-    assert np.allclose(af._x_deconv[scatter], tests['{0:g}'.format(scatter)], rtol=0.01)
+    assert np.allclose(af._x_deconv[scatter], tests['{0:g}'.format(scatter)], rtol=0.001)
 
     # assert (af._x_deconv[scatter] == tests['{0:g}'.format(scatter)]).all()
     # diff = (af._x_deconv[scatter] == tests['{0:g}'.format(scatter)])
